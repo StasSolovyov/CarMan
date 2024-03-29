74085bed95dc568deef4269dd5eb389d667b3ab1
StasSolovyov <149395885+StasSolovyov@users.noreply.github.com>
2024-03-29 15:35:45 +0300

    finish version

---
+++ config.js
@@ -0,0 +1,9 @@
+const BOT_TOKEN = '6994125913:AAGtOf28P5GIzjrIPN5XcywiOWHT8SuwoXM';
+const CHAT_ID_1 = '560754686';
+const CHAT_ID_2 = '833599085';
+
+ports = {
+    BOT_TOKEN,
+    CHAT_ID_1,
+    CHAT_ID_2,
+};
--- env.js
+++
@@ -1,9 +0,0 @@
-const BOT_TOKEN = process.env.BOT_TOKEN;
-const CHAT_ID_1 = process.env.CHAT_ID_1;
-const CHAT_ID_2 = process.env.CHAT_ID_2;
-
-module.exports = {
-    BOT_TOKEN,
-    CHAT_ID_1,
-    CHAT_ID_2,
-};
