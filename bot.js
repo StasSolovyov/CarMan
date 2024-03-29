afbc85111d62bfcdc8e1f08aee11404cbf341b27
StasSolovyov <149395885+StasSolovyov@users.noreply.github.com>
2024-03-29 17:39:10 +0300

    finish version00

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf');
 const setupHandlers = require('./handlers');
-const { BOT_TOKEN } = require('./constants');
+const { BOT_TOKEN } = require('./config');
 
 const bot = new Telegraf(BOT_TOKEN);
 
--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const BOT_TOKEN = '6994125913:AAGtOf28P5GIzjrIPN5XcywiOWHT8SuwoXM';
 const CHAT_ID_1 = '560754686';
 const CHAT_ID_2 = '833599085';
 
-ports = {
+module.exports = {
     BOT_TOKEN,
     CHAT_ID_1,
     CHAT_ID_2,
